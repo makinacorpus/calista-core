15d8be1e331b3d0724a2286036f314691cc1c72b
Pierre Rineau <pierre.rineau@makina-corpus.com>
2018-12-12 13:57:11 +0100

    minor cleanup

--- calista-bundle/src/DependencyInjection/CalistaExtension.php
+++ calista-bundle/src/DependencyInjection/CalistaExtension.php
@@ -8,10 +8,9 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 
 /**
  * @codeCoverageIgnore
